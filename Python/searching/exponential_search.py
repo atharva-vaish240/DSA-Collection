721e36de2767b9f6160d0a5a1af36ddfaf0982f8
Harsh Pareshbhai Gosaliya <153543231+harshpareshbhaigosalya@users.noreply.github.com>
2025-10-02 14:51:02 +0530

    Create exponential_search.py
    add new searching technique under python folder

---
+++ Python/searching/exponential_search.py
@@ -0,0 +1,18 @@
+def binary_search(arr, target, low, high):
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+def exponential_search(arr, target):
+    if arr[0] == target:
+        return 0
+    i = 1
+    while i < len(arr) and arr[i] <= target:
+        i *= 2
+    return binary_search(arr, target, i // 2, min(i, len(arr)-1))
