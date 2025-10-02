0e45523adeea923d389c0182a4db086bafaf91e3
Vipanshu <vipu3104@gmail.com>
2025-10-02 13:01:23 +0530

    Add main function and example to Best Time to Buy and Sell Stock
    Added headers, a main function, and an example usage to the Best Time to Buy and Sell Stock.cpp file for demonstration and testing purposes.

--- CPP/dynamic-programming/Best Time to Buy and Sell Stock.cpp
+++ CPP/dynamic-programming/Best Time to Buy and Sell Stock.cpp
@@ -1,10 +1,17 @@
-class Solution {
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
 public:
-    int maxProfit(vector<int>& prices) {
+    int maxProfit(vector<int> &prices)
+    {
         int n = prices.size();
         int maxp = 0;
         int mini = prices[0];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             int curProfit = prices[i] - mini;
             maxp = max(maxp, curProfit);
             mini = min(mini, prices[i]);
@@ -12,3 +19,12 @@ public:
         return maxp;
     }
 };
+
+int main()
+{
+    vector<int> prices = {7, 1, 5, 3, 6, 4};
+    Solution sol;
+    int result = sol.maxProfit(prices);
+    cout << "Maximum Profit: " << result << endl;
+    return 0;
+}
