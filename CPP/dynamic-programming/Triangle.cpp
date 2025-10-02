120b239357844f4ece3047fdcf490b6c190df620
Umer-Jahangir <umerjahangir.cs@gmail.com>
2025-10-02 15:31:40 +0500

    Fix: added missing headers for vector and main()

--- CPP/dynamic-programming/Triangle.cpp
+++ CPP/dynamic-programming/Triangle.cpp
@@ -1,13 +1,12 @@
+#include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<int> t = triangle[n-1]; // start from last row
-
+        vector<int> t = triangle[n-1];
         for(int row = n - 2; row >= 0; row--) {
             for(int col = 0; col <= row; col++) {
                 t[col] = triangle[row][col] + min(t[col], t[col + 1]);
@@ -16,3 +15,15 @@ public:
         return t[0];
     }
 };
+
+int main() {
+    Solution sol;
+    vector<vector<int>> triangle = {
+        {2},
+        {3,4},
+        {6,5,7},
+        {4,1,8,3}
+    };
+    cout << sol.minimumTotal(triangle) << endl; // Output should be 11
+    return 0;
+}
