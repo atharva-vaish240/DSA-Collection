f76c7b278e1b3c3bf10377d8176f4225a74549e5
Aviral Jain <74827110+Aviral1-jain@users.noreply.github.com>
2025-10-02 13:40:38 +0530

    Create timsort.py

---
+++ Python/sorting/timsort.py
@@ -0,0 +1,9 @@
+# Using the sort() method
+a = [5, 3, 1, 4, 6, 2]
+a.sort()
+print("Sorted list using sort():", a)
+
+# Using the sorted() function
+a = [5, 3, 1, 4, 6, 2]
+sorted_list = sorted(a)
+print("Sorted list using sorted():", sorted_list)
