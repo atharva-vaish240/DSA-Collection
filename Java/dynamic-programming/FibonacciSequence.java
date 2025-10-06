a36313f5560adcf1d745ec65cb886178eb5a5ed7
Nishagi <jeewanthadeherath04@gmail.com>
2025-10-06 09:59:05 +0530

    added the dp problem fibonacci sequence in Java

---
+++ Java/dynamic-programming/FibonacciSequence.java
@@ -0,0 +1,22 @@
+public class FibonacciSequence {
+    public static void main(String[] args) {
+        int n = 10; // change this value to generate more or fewer terms
+        System.out.println("Fibonacci Sequence up to " + n + " terms:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int[] fib = new int[n + 1];
+        fib[0] = 0;
+        fib[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+        return fib[n];
+    }
+}
